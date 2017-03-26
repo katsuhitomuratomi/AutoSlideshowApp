e17d4825522eade84ae0c4ad6c232ac53eb813ef
katsuhitomuratomi <garo0713@icloud.com>
2017-03-26 23:28:55 +0900

    commit2

--- app/src/main/java/jp/techacademy/katsuhito/muratomi/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/katsuhito/muratomi/autoslideshowapp/MainActivity.java
@@ -8,21 +8,34 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private static final int PERMISSIONS_REQUES_CODE = 100;
+    Timer mtimer;
+    Handler mhandler = new Handler();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1 = (Button) findViewById(R.id.back);
+        Button button2 = (Button) findViewById(R.id.saisei);
+        Button button3 = (Button) findViewById(R.id.step);
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
@@ -31,6 +44,14 @@ public class MainActivity extends AppCompatActivity {
                 requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUES_CODE);
             }
         }
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
     }
@@ -63,12 +84,29 @@ public class MainActivity extends AppCompatActivity {
             do {
                 int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
                 Long id = cursor.getLong(fieldIndex);
-                Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+                final Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
                 Log.d("test", "URI" + imageUri.toString());
-                ImageView imageView = (ImageView) findViewById(R.id.imageView);
-                imageView.setImageURI(imageUri);
+                final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+
+                mtimer = new Timer();
+                mtimer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        mhandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                imageView.setImageURI(imageUri);
+                            }
+                        });
+                    }
+                }, 100, 2000);
+
+
             } while (cursor.moveToNext());
         }
         cursor.close();
     }
+
+
 }

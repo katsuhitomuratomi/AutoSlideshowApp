d991a697c3038b51a2b1445349e1bb14b606ede2
katsuhitomuratomi <garo0713@icloud.com>
2017-03-27 16:27:51 +0900

    commit3

--- app/src/main/java/jp/techacademy/katsuhito/muratomi/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/katsuhito/muratomi/autoslideshowapp/MainActivity.java
@@ -16,7 +16,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -24,9 +24,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     private static final int PERMISSIONS_REQUES_CODE = 100;
-    Timer mtimer;
-    Handler mhandler = new Handler();
-
+    Timer timer;
+    Handler handler = new Handler();
+    int i = 0;
+    ArrayList<Uri> arrayList = new ArrayList<Uri>();
+    ImageView imageView = (ImageView) findViewById(R.id.imageView);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,12 +48,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+
             }
         });
+
+
 
 
     }
@@ -81,29 +86,39 @@ public class MainActivity extends AppCompatActivity {
         );
 
         if (cursor.moveToFirst()) {
+
+
+            Uri imageUri;
             do {
                 int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
                 Long id = cursor.getLong(fieldIndex);
-                final Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+                imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+                arrayList.add(imageUri);
                 Log.d("test", "URI" + imageUri.toString());
-                final ImageView imageView = (ImageView) findViewById(R.id.imageView);
-
-
-                mtimer = new Timer();
-                mtimer.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        mhandler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                imageView.setImageURI(imageUri);
-                            }
-                        });
-                    }
-                }, 100, 2000);
 
 
             } while (cursor.moveToNext());
+            timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+
+
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            int j = i % 3;
+
+                            imageView.setImageURI(arrayList.get(j));
+                            i++;
+
+                        }
+                    });
+                }
+            }, 100, 2000);
+
+
         }
         cursor.close();
     }
